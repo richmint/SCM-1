9ff4b8a8b6ad70fd99e54de24ce8c59770ae3672
Vishnu sharma <vishnu@razorse.com>
2022-06-30 12:44:52 +0530

    Deleting old src folder

--- react-admin/src@old/appReducer.js
+++
@@ -1,9 +0,0 @@
-// import { combineReducers } from 'redux';
-// import { centerReducer as centerPage } from './centerReducer';
-// const appReducer = combineReducers({
-    
-//     centerPage,
-
-// });
-
-// export default appReducer;
--- react-admin/src@old/centerActionType.js
+++
@@ -1,3 +0,0 @@
-// export const GET_CENTER = "GET_CENTER";
-// export const GET_CENTER_SUCCESS = "GET_CENTER_SUCCESS";
-// export const GET_CENTER_FAILURE = "GET_CENTER_FAILURE";
